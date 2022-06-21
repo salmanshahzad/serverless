02ef72b3918e02286e58bf876be5908635bf6a63
Salman Shahzad <salshahzad@outlook.com>
2022-06-20 21:24:22 -0400

    Initial commit

---
+++ packages/serverless/foo/index.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export async function main(args) {
+    console.log(process.env);
+    console.log(args);
+    const res = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
+    console.log(res.data);
+    return { body: res.data };
+}
